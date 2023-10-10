6726bcd8d62c70e6a5a102ccf76687ec7509b9da
boileroman <sonozakifiora@gmail.com>
2023-10-10 16:00:10 +0500

    commit

---
+++ postcss.config.cjs
@@ -0,0 +1,5 @@
+const postcssNessted = require("postcss-nested")
+const postcssPresetEnv = require("postcss-preset-env")
+const postcssCustomMedia = require("postcss-custom-media")
+
+module.exports={plugins: [postcssNessted(), postcssPresetEnv(), postcssCustomMedia({})]}
